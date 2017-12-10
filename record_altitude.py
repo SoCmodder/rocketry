7a381aef0094463446de2c1923e6ec63ce70e536
Mitch Miller <socmodder@gmail.com>
2017-12-10 11:56:10 -0500

    formatting

--- record_altitude.py
+++ record_altitude.py
@@ -14,11 +14,13 @@ i = 0
 f = open("data.txt","w") 
 
 for i in range(0, 300):
+	f.write('======================================================================\n')
 	f.write('Timestamp: {:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()) + '\n')
 	f.write('Temp = {0:0.2f} *C'.format(sensor.read_temperature()) + '\n')
 	f.write('Pressure = {0:0.2f} Pa'.format(sensor.read_pressure()) + '\n')
 	f.write('Altitude = {0:0.2f} m'.format(sensor.read_altitude()) + '\n')
 	f.write('Sealevel Pressure = {0:0.2f} Pa'.format(sensor.read_sealevel_pressure()) + '\n')
+	f.write('======================================================================\n\n')
 	time.sleep(1)
 
 #Save/Close the file
