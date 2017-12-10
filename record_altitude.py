f68ad81ceaaf2e7deb9d4317446395c9f4b2a22d
Mitch Miller <socmodder@gmail.com>
2017-12-10 11:37:22 -0500

    add newline to record_altitude.py

--- record_altitude.py
+++ record_altitude.py
@@ -13,10 +13,10 @@ i = 0
 f = open("data.txt","w") 
 
 for i in range(0, 300):
-	f.write('Temp = {0:0.2f} *C'.format(sensor.read_temperature()))
-	f.write('Pressure = {0:0.2f} Pa'.format(sensor.read_pressure()))
-	f.write('Altitude = {0:0.2f} m'.format(sensor.read_altitude()))
-	f.write('Sealevel Pressure = {0:0.2f} Pa'.format(sensor.read_sealevel_pressure()))
+	f.write('Temp = {0:0.2f} *C'.format(sensor.read_temperature()) + '\n')
+	f.write('Pressure = {0:0.2f} Pa'.format(sensor.read_pressure()) + '\n')
+	f.write('Altitude = {0:0.2f} m'.format(sensor.read_altitude()) + '\n')
+	f.write('Sealevel Pressure = {0:0.2f} Pa'.format(sensor.read_sealevel_pressure()) + '\n')
 	time.sleep(1)
 
 #Save/Close the file
