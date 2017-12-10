de0856bfabf6dd7a55fdb9d19d9b330cc52825d2
Mitch Miller <socmodder@gmail.com>
2017-12-10 11:21:32 -0500

    record altitude updates

--- record_altitude.py
+++ record_altitude.py
@@ -2,18 +2,22 @@
 # Author: Mitch Miller <socmodder@gmail.com>
 import logging
 import BMP280 as BMP280
+import time
 
 logging.basicConfig(level=logging.DEBUG)
 
 sensor = BMP280.BMP280()
 
 # Open File
+i = 0
 f = open(“data.txt”,”w”) 
 
-f.write('Temp = {0:0.2f} *C'.format(sensor.read_temperature()))
-f.write('Pressure = {0:0.2f} Pa'.format(sensor.read_pressure()))
-f.write('Altitude = {0:0.2f} m'.format(sensor.read_altitude()))
-f.write('Sealevel Pressure = {0:0.2f} Pa'.format(sensor.read_sealevel_pressure()))
+for i in range(0, 300):
+	f.write('Temp = {0:0.2f} *C'.format(sensor.read_temperature()))
+	f.write('Pressure = {0:0.2f} Pa'.format(sensor.read_pressure()))
+	f.write('Altitude = {0:0.2f} m'.format(sensor.read_altitude()))
+	f.write('Sealevel Pressure = {0:0.2f} Pa'.format(sensor.read_sealevel_pressure()))
+	time.sleep(1)
 
 #Save/Close the file
 f.close()
