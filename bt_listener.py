a62ae4d0402a4ac6b1d87ff94404580c970994dc
Mitch Miller <socmodder@gmail.com>
2017-12-23 20:04:49 -0500

    bt

--- bt_listener.py
+++ bt_listener.py
@@ -1,17 +1,13 @@
-import picamera
 from bluetooth import *
 import RPi.GPIO as GPIO        #calling for header file which helps in using GPIOs of PI
+import picamera
 import datetime
 import time
 import threading
 import BMP280 as BMP280
 import logging
 
-logging.basicConfig(level=logging.DEBUG)
-
 sensor = BMP280.BMP280()
-
-LED=21
 
 GPIO.setmode(GPIO.BCM)     #programming the GPIO by BCM pin numbers. (like PIN40 as GPIO21)
 GPIO.setwarnings(False)
@@ -22,14 +18,14 @@ camera = picamera.PiCamera()
 camera.resolution = (1280, 720)
 
 server_socket=BluetoothSocket( RFCOMM )
-
+ 
 port = 1
-server_socket.bind(("",PORT_ANY))
+server_socket.bind(("",port))
 server_socket.listen(1)
 
 uuid = "fa87c0d0-afac-11de-8a39-0800200c9a66"
 
-advertise_service( server_sock, "RSV",
+advertise_service( server_socket, "RSV",
                    service_id = uuid,
                    service_classes = [ uuid, SERIAL_PORT_CLASS ],
                    profiles = [ SERIAL_PORT_PROFILE ], 
@@ -39,59 +35,57 @@ advertise_service( server_sock, "RSV",
 threads = []
 
 def altworker():
-	"""thread worker function"""
-	f = open("bt-activated-data-recording.txt","w") 
-	for i in range(0, 960):
-		f.write('======================================================================\n')
-		f.write('Timestamp: {:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()) + '\n')
-		f.write('Temp = {0:0.2f} *C'.format(sensor.read_temperature()) + '\n')
-		f.write('Pressure = {0:0.2f} Pa'.format(sensor.read_pressure()) + '\n')
-		f.write('Altitude = {0:0.2f} m'.format(sensor.read_altitude()) + '\n')
-		f.write('Sealevel Pressure = {0:0.2f} Pa'.format(sensor.read_sealevel_pressure()) + '\n')
-		f.write('======================================================================\n\n')
-		time.sleep(1)
-	#Save/Close the file
-	f.close()
-	client_socket.send("Finished Altitude Data Recording!\n")
-	return 
+  """thread worker function"""
+  f = open("bt-activated-data-recording.txt","w") 
+  for i in range(0, 960):
+    f.write('======================================================================\n')
+    f.write('Timestamp: {:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()) + '\n')
+    f.write('Temp = {0:0.2f} *C'.format(sensor.read_temperature()) + '\n')
+    f.write('Pressure = {0:0.2f} Pa'.format(sensor.read_pressure()) + '\n')
+    f.write('Altitude = {0:0.2f} m'.format(sensor.read_altitude()) + '\n')
+    f.write('Sealevel Pressure = {0:0.2f} Pa'.format(sensor.read_sealevel_pressure()) + '\n')
+    f.write('======================================================================\n\n')
+    time.sleep(1)
+  #Save/Close the file
+  f.close()
+  client_socket.send("Finished Altitude Data Recording!\n")
+  return 
 
 def vidworker():
-	"""thread worker function"""
-	camera.start_recording('bt-activated-video.h264')
-	camera.wait_recording(960)
-	camera.stop_recording()
-	client_socket.send("Finished Recording Video")
-	return
+  """thread worker function"""
+  camera.start_recording('bt-activated-video.h264')
+  camera.wait_recording(960)
+  camera.stop_recording()
+  client_socket.send("Finished Recording Video")
+  return
 
+initAlt = sensor.read_altitude()  
+ 
+client_socket,address = server_socket.accept()
 print "Accepted connection from ",address
+while 1:
+ 
+  data = client_socket.recv(1024)
+  print "Received: %s" % data
 
-initAlt = sensor.read_altitude()
-
-while True:
-
-	client_socket,address = server_socket.accept()
-
-	data = client_socket.recv(1024)
-	print "Received: %s" % data
-	if (data == "1"):    #if '1' is sent from the Android App, turn OFF the LED
-		client_socket.send("Recording Video!\n")
-		t = threading.Thread(target=vidworker)
-		threads.append(t)
-		t.start()
-	if (data == "2"):
-		client_socket.send("Recording Data!\n")
-		t2 = threading.Thread(target=altworker)
-		threads.append(t2)
-		t2.start()		
-	if (data == "q"):
-		print ("Quit")
-		client_socket.send("Quit command accepted: Shutting Down All Recording!")
-		break
-	client_socket.send('Altitude = {0:0.2f} m'.format(sensor.read_altitude() - initAlt) + '\n')
+  if (data == "1"):    #if '0' is sent from the Android App, turn OFF the LED
+    client_socket.send("Recording Video!\n")
+    t = threading.Thread(target=vidworker)
+    threads.append(t)
+    t.start()
+  if (data == "2"):    #if '1' is sent from the Android App, turn OFF the LED
+    client_socket.send("Recording Data!\n")
+    t2 = threading.Thread(target=altworker)
+    threads.append(t2)
+    t2.start()
+  if (data == "q"):
+    print ("Quit")
+    client_socket.send("Quit command accepted: Shutting Down All Recording!")
+    break
+  client_socket.send('Altitude = {0:0.2f} m'.format(sensor.read_altitude() - initAlt) + '\n')
 
 camera.close()
 f.close()
 
 client_socket.close()
 server_socket.close()
-
