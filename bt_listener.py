00a1719bc98da00e6b356827b0facaf06467076d
Mitch Miller <socmodder@gmail.com>
2017-12-23 20:43:09 -0500

    bt

--- bt_listener.py
+++ bt_listener.py
@@ -21,38 +21,38 @@ server_socket.listen(1)
 uuid = "fa87c0d0-afac-11de-8a39-0800200c9a66"
 
 advertise_service( server_socket, "RSV",
-                   service_id = uuid,
-                   service_classes = [ uuid, SERIAL_PORT_CLASS ],
-                   profiles = [ SERIAL_PORT_PROFILE ], 
+									 service_id = uuid,
+									 service_classes = [ uuid, SERIAL_PORT_CLASS ],
+									 profiles = [ SERIAL_PORT_PROFILE ], 
 #                   protocols = [ OBEX_UUID ] 
-                    )
+										)
 
 threads = []
 
 def altworker():
-  """thread worker function"""
-  f = open("bt-activated-data-recording.txt","w") 
-  for i in range(0, 960):
-    f.write('======================================================================\n')
-    f.write('Timestamp: {:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()) + '\n')
-    f.write('Temp = {0:0.2f} *C'.format(sensor.read_temperature()) + '\n')
-    f.write('Pressure = {0:0.2f} Pa'.format(sensor.read_pressure()) + '\n')
-    f.write('Altitude = {0:0.2f} m'.format(sensor.read_altitude()) + '\n')
-    f.write('Sealevel Pressure = {0:0.2f} Pa'.format(sensor.read_sealevel_pressure()) + '\n')
-    f.write('======================================================================\n\n')
-    time.sleep(1)
-  #Save/Close the file
-  f.close()
-  client_socket.send("Finished Altitude Data Recording!\n")
-  return 
+	"""thread worker function"""
+	f = open("bt-activated-data-recording.txt","w") 
+	for i in range(0, 960):
+		f.write('======================================================================\n')
+		f.write('Timestamp: {:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()) + '\n')
+		f.write('Temp = {0:0.2f} *C'.format(sensor.read_temperature()) + '\n')
+		f.write('Pressure = {0:0.2f} Pa'.format(sensor.read_pressure()) + '\n')
+		f.write('Altitude = {0:0.2f} m'.format(sensor.read_altitude()) + '\n')
+		f.write('Sealevel Pressure = {0:0.2f} Pa'.format(sensor.read_sealevel_pressure()) + '\n')
+		f.write('======================================================================\n\n')
+		time.sleep(1)
+	#Save/Close the file
+	f.close()
+	client_socket.send("Finished Altitude Data Recording!\n")
+	return 
 
 def vidworker():
-  """thread worker function"""
-  camera.start_recording('bt-activated-video.h264')
-  camera.wait_recording(960)
-  camera.stop_recording()
-  client_socket.send("Finished Recording Video")
-  return
+	"""thread worker function"""
+	camera.start_recording('bt-activated-video.h264')
+	camera.wait_recording(960)
+	camera.stop_recording()
+	client_socket.send("Finished Recording Video")
+	return
 
 initAlt = sensor.read_altitude()  
  
@@ -61,34 +61,33 @@ print "Accepted connection from ",address
 
 try:
 	while 1:
- 
-	  data = client_socket.recv(1024)
-	  print "Received: %s" % data
+		data = client_socket.recv(1024)
+		print "Received: %s" % data
 
-	  if (data == "1"):    #if '0' is sent from the Android App, turn OFF the LED
-	    client_socket.send("Recording Video!\n")
-	    t = threading.Thread(target=vidworker)
-	    threads.append(t)
-	    t.start()
-	  if (data == "2"):    #if '1' is sent from the Android App, turn OFF the LED
-	    client_socket.send("Recording Data!\n")
-	    t2 = threading.Thread(target=altworker)
-	    threads.append(t2)
-	    t2.start()
-	  if (data == "q"):
-	    print ("Quit")
-	    client_socket.send("Quit command accepted: Shutting Down All Recording!")
-	    break
-	  client_socket.send('Altitude = {0:0.2f} m'.format(sensor.read_altitude() - initAlt) + '\n')
+		if (data == "1"):    #if '0' is sent from the Android App, turn OFF the LED
+			client_socket.send("Recording Video!\n")
+			t = threading.Thread(target=vidworker)
+			threads.append(t)
+			t.start()
+		if (data == "2"):    #if '1' is sent from the Android App, turn OFF the LED
+			client_socket.send("Recording Data!\n")
+			t2 = threading.Thread(target=altworker)
+			threads.append(t2)
+			t2.start()
+		if (data == "q"):
+			print ("Quit")
+			client_socket.send("Quit command accepted: Shutting Down All Recording!")
+			break
+		client_socket.send('Altitude = {0:0.2f} m'.format(sensor.read_altitude() - initAlt) + '\n')
 
 except KeyboardInterrupt:  
-    # here you put any code you want to run before the program   
-    # exits when you press CTRL+C  
-    camera.close()
+		# here you put any code you want to run before the program   
+		# exits when you press CTRL+C  
+	camera.close()
 	f.close()
 	client_socket.close()
 	server_socket.close()
-    print "\n", counter # print value of counter
+	print "\n", counter # print value of counter
 
 finally:  
-    GPIO.cleanup() # this ensures a clean exit     
+	GPIO.cleanup() # this ensures a clean exit     
