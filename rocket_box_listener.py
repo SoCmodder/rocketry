670e7c0d6aae544f3471db7eb88b0b5730e46445
Mitch Miller <socmodder@gmail.com>
2018-01-13 18:52:02 -0500

    testing

--- rocket_box_listener.py
+++ rocket_box_listener.py
@@ -11,17 +11,8 @@ port = 1
 socket.connect((bd_addr, port))
 socket.send("RSV BT Connection Established")
 
-try:
-	while 1:
-		#data = socket.recv(1024)
-		socket.send("Sending Data")
-		time.sleep(1)
-
-except KeyboardInterrupt:  
-		# here you put any code you want to run before the program   
-		# exits when you press CTRL+C  
-	#camera.close()
-	#f.close()
-
-finally:  
-	GPIO.cleanup() # this ensures a clean exit     
+while 1:
+	#data = socket.recv(1024)
+	socket.send("Sending Data")
+	time.sleep(1)
+	
