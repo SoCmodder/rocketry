2d8d6119fd4c9bf7f288881a814ea224b7edec6c
Mitch Miller <socmodder@gmail.com>
2018-01-13 19:59:39 -0500

    testing

--- rocket_box_listener.py
+++ rocket_box_listener.py
@@ -2,18 +2,19 @@ from bluetooth import *
 import RPi.GPIO as GPIO        #calling for header file which helps in using GPIOs of PI
 import time
 import logging
-
-socket=BluetoothSocket( RFCOMM )
  
 bd_addr = "98:D3:31:50:3A:86" #Rocketbox Address
 port = 1
 
-socket.connect((bd_addr, port))
-socket.send("RSV Connected")
-time.sleep(2)
-
 while 1:
-	#data = socket.recv(1024)
-	socket.send("Sending Data")
-	time.sleep(3)
-	
+	socket=BluetoothSocket( RFCOMM )
+	socket.connect((bd_addr, port))
+	socket.send("RSV Connected")
+	time.sleep(2)
+
+	try:
+		while 1:
+			socket.send("Sending Data")
+			time.sleep(3)
+	finally:
+		socket.close()	
